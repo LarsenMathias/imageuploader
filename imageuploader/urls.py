57b9224255166d764c496d0f10a832a7273a7a0b
Dark Lord <larsenm2020@gmail.com>
2021-09-10 18:36:19 +0530

    commit

--- imageuploader/urls.py
+++ imageuploader/urls.py
@@ -18,6 +18,8 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from myapp import views
+from django.views.static import serve
+from django.conf.urls import url 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
@@ -27,4 +29,6 @@ urlpatterns = [
     path('Controversy',views.Controversy,name='Controversy'),
     path('Actor',views.Actor,name='Actor'),
     path('education',views.education,name='education'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
