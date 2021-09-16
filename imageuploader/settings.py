a7d0bb102ebf2b30ee7a14c5fc1c46c4c67a6500
LarsenMathias <larsenm2020@gmail.com>
2021-09-16 21:26:06 +0530

    commit

--- imageuploader/settings.py
+++ imageuploader/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,8 +133,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles')) 
 if not os.path.exists(STATIC_ROOT):
     os.mkdir(STATIC_ROOT)
-# STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'), )
-# django_heroku.settings(locals())
-if 'DATABASE_URL' in os.environ:
-    import dj_database_url
-    DATABASES = {'default': dj_database_url.config()}
+STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'), )
+django_heroku.settings(locals())
+# if 'DATABASE_URL' in os.environ:
+#     import dj_database_url
+#     DATABASES = {'default': dj_database_url.config()}
